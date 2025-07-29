6940d3ecca1b09c622cb5a67cab1c15c812b6a82
Marcelo <marcelodossantosmachado05@gmail.com>
2025-07-29 16:41:26 -0300

    teste de trabalho em equipe

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-const produto = [
-  { nome: "", categoria: "", preco: 29.9 },
-  { nome: "", categoria: "", preco: 29.9 },
-  { nome: "", categoria: "", preco: 29.9 },
-  { nome: "", categoria: "", preco: 29.9 },
+const arrayProduto = [
+  { nome: "perfume", categoria: "perfume", preco: 29.9 },
+  { nome: "shampoo", categoria: "shampoo", preco: 29.9 },
+  { nome: "sabonete", categoria: "sabonete", preco: 29.9 },
+  { nome: "difusor aromatico", categoria: "difusorAroma", preco: 29.9 },
   { nome: "", categoria: "", preco: 29.9 },
   { nome: "", categoria: "", preco: 29.9 },
   { nome: "", categoria: "", preco: 29.9 },
@@ -17,8 +17,8 @@ function gerarCards(produto) {
     .map(
       (prod) => `
     <div class="card mb-5 col-md-4 col-lg-3 border border-light rounde rounded-3 text-center bg-dark text-light p-0 pb-3">
-    <img src=${prod.nome} class="image-fluid rounded rounded-3" style="height:200px">
-      ${prod.categoria}</h2>
+    <h2>${prod.nome}</h2>
+      Categoria: ${prod.categoria}<br>
       Preço: ${prod.preco}<br>
     </div>
   `
@@ -30,14 +30,14 @@ function listarProdutos() {
   const selectValue = document.getElementById("selectID").value;
   const valor = selectValue.toLowerCase();
 
-  const filtrados = selectValue
-    ? arrayProdutos.filter((filtrar) => filtrar.marca.toLowerCase() === valor)
-    : arrayProdutos;
+  const filtrado = selectValue
+    ? arrayProduto.filter((filtrar) => filtrar.categoria.toLowerCase() === valor)
+    : arrayProduto;
 
-  gerarCards(filtrados);
+  gerarCards(filtrado);
 }
 
 window.addEventListener("DOMContentLoaded", () => {
   document.getElementById("botaoID").addEventListener("click", listarProdutos);
-  listarProdutos(); // exibe todos no carregamento
+  listarProdutos();
 });
